745443bfd1139ba649afee8430e3c0dafe82f86f
Alberto Pedron <pedron.albert@gmail.com>
2017-06-02 05:08:15 -0400

    Add async to adsense

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ module.exports = {
       { rel: 'alternate', hreflang: 'x-default', href: 'https://www.coloso.net' },
     ],
     script: [
-      { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' },
+      { src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js', async: 'async' },
     ],
   },
 
